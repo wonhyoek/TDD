396345c76fdb53a0309120775147f2f552d81ad2
wonhyoek <epahsejtmxm@naver.com>
2021-01-17 22:06:01 +0900

    Setting 2

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 5000;
+const productRoutes = require('./Router');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-    res.send('HELLO')
-})
+mongoose.connect('mongodb+srv://wonhyoek:ab8975@cluster0.irlkt.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+    .catch((err) => console.log(err));
+
+
+
+app.use(express.json());
+
+
+app.use('/api/products', productRoutes);
+
 
 app.listen(port, () => 
     console.log(`Server running on http://localhost:${port}`));
